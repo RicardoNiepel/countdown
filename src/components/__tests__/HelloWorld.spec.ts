de58ebd776e95b89ebb13c3b7299aec5e53c1179
Ricardo Niepel <RicardoNiepel@users.noreply.github.com>
2024-02-23 14:43:31 +0100

    initial commit

---
+++ src/components/__tests__/HelloWorld.spec.ts
@@ -0,0 +1,21 @@
+import { describe, it, expect, vi } from 'vitest'
+import { mount } from '@vue/test-utils'
+import { createTestingPinia } from '@pinia/testing'
+
+import HelloWorld from '../HelloWorld.vue'
+
+describe('HelloWorld', () => {
+  it('renders properly', () => {
+    const wrapper = mount(HelloWorld, {
+      props: { msg: 'Hello Vitest' },
+      global: {
+        plugins: [
+          createTestingPinia({
+            createSpy: vi.fn
+          })
+        ]
+      }
+    })
+    expect(wrapper.text()).toContain('Hello Vitest')
+  })
+})
